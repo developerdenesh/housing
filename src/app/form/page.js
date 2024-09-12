20981539e397a23d36595898aef8ca1fdf4d0bba
runner_bot <runner@runner.com>
2024-09-12 22:24:03 +0800

    Add ag grid to form endpoint

--- src/app/form/page.js
+++ src/app/form/page.js
@@ -1,26 +1,47 @@
 'use client'
 
-import { Button, Group, TextInput } from '@mantine/core';
-import { useForm } from '@mantine/form';
+import { AgGridReact } from 'ag-grid-react';
+import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the Data Grid
+import "ag-grid-community/styles/ag-theme-quartz.css"; // Optional Theme applied to the Data Grid
+import { useState } from 'react';
 
 export default function Demo() {
-  const form = useForm({
-    mode: 'uncontrolled',
-    initialValues: {
-      email: '',
-      termsOfService: false,
+  // Row Data: The data to be displayed.
+  const [rowData, setRowData] = useState([
+    { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+    { make: "Ford", model: "F-Series", price: 33850, electric: false },
+    { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+  ]);
+
+  // Column Definitions: Defines the columns to be displayed.
+  const [colDefs, setColDefs] = useState([
+    {
+      field: "make",
+      cellRenderer(props) {
+        console.log(props)
+        return <button onClick={() => window.alert('clicked') }>{props.data.make}</button>;
+      }
     },
-  });
+    { 
+      field: "model" 
+    },
+    { 
+      field: "price" 
+    },
+    { 
+      field: "electric" 
+    },
+  ]);
 
   return (
-    <form onSubmit={form.onSubmit((values) => console.log(values))}>
-      <TextInput
-        placeholder="14280000"
-        key={form.key('email')}
-        {...form.getInputProps('email')}
+    <div
+      className="ag-theme-quartz" // applying the Data Grid theme
+      style={{ height: 500 }} // the Data Grid will fill the size of the parent container
+    >
+      <AgGridReact
+        rowData={rowData}
+        columnDefs={colDefs}
       />
-
-        <Button fullWidth type="submit" variant="filled" color="green">Submit</Button>
-    </form>
+    </div>
   );
 }
