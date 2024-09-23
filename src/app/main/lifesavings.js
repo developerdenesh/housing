8415144b76a1192d7fbde21b211e3ae6b4491661
runner_bot <runner@runner.com>
2024-09-23 21:44:52 +0800

    Take into account the starting year for monthly expenses

--- src/app/main/lifesavings.js
+++ src/app/main/lifesavings.js
@@ -7,30 +7,38 @@ const LifeSavings = (props) => {
 
     useEffect(() => {
         const arr = []
-        for (let i = 2024; i <= 2024 + 30; i +=1) {
-                const yearExpenditure = []
-                expenditure.map((element) => {
-                    if (element.type === false) {
-                        yearExpenditure.push({
-                            name: element.name,
-                            type: "single",
-                            year: element.year,
-                            bill: element.bill
-                        })
+        console.error(expenditure)
+        for (let i = 2024; i <= 2024 + 30; i += 1) {
+            const yearExpenditure = []
+            expenditure.map((element) => {
+                if (element.type === false) {
+                    yearExpenditure.push({
+                        name: element.name,
+                        type: "single",
+                        year: element.year,
+                        bill: element.bill
+                    })
+                    return element
+                }
+                if (element.name === "cake")
+                    console.error(`The name: ${element.name}, type: monthly, bill: ${element.bill}, bool: ${element.timeframe >= i - 2024}, year: ${element.year}`)
 
-                        return element
-                    }
-
-                    if (element.timeframe >= i - 2024) {
-                        yearExpenditure.push({
-                            name: element.name,
-                            type: "monthly",
-                            timeframe: element.timeframe - i + 2024,
-                            bill: element.bill
-                        })
-                    }
-                })
-                arr.push(<RecipeReviewCard key={i} year={i} expenditure={yearExpenditure} />)
+                if (element.timeframe >= i - element.year && i >= element.year) {
+                    yearExpenditure.push({
+                        name: element.name,
+                        type: "monthly",
+                        bill: element.bill,
+                        timeframe: element.timeframe - (i - element.year),
+                    })
+                }
+            })
+            arr.push(
+                <RecipeReviewCard
+                    key={i}
+                    year={i}
+                    expenditure={yearExpenditure}
+                />
+            )
         }
         setRecipieReviewCards(arr)
     }, [expenditure])
